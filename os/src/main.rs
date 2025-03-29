5c31563a9e85fb11e9bf0cd821dddaf9b1512458
Terry <hl928452957@gmail.com>
2025-03-29 16:07:50 +0800

    test

--- os/src/main.rs
+++ os/src/main.rs
@@ -1,33 +1,43 @@
+
 #![no_std]
 #![no_main]
 
+use crate::sbi::{shutdown};
+use crate::syscall::{sys_exit,print};
+
+
 mod lang_items;
+mod sbi;
+mod syscall;
 
+core::arch::global_asm!(include_str!("entry.asm"));
 
-
-
-
-const SYSCALL_EXIT: usize = 93;
-fn syscall(id:usize,args:[usize;3])->isize{
-    let mut ret;
-    unsafe{
-        core::arch::asm!(
-        "ecall",
-        inlateout("x10") args[0] => ret,
-            in("x11" ) args[1],
-            in("x12" ) args[2],
-            in("x17" ) id,
-        );
+fn clear_bss(){
+    extern "C" {
+        fn sbss();
+        fn ebss();
     }
-    ret
+    (sbss as usize..ebss as usize).for_each(|a|{
+        unsafe {
+            (a as *mut u8).write_volatile(0)
+        }
+    });
 }
 
-pub fn sys_exit(xstate:i32)->isize{
-    syscall(SYSCALL_EXIT,[xstate as usize,0,0])
-}
 
-/// no_mangle is used to prevent the Rust compiler from changing the name of the function
-#[unsafe(no_mangle)]
-extern "C" fn _start(){
-    sys_exit(9);
+// // no_mangle is used to prevent the Rust compiler from changing the name of the function
+// #[no_mangle]
+// extern "C" fn _start() {
+//     println!("Hello, world!");
+//     sys_exit(9);
+//     // shutdown();
+// }
+#[no_mangle]
+pub fn rust_main(){
+    clear_bss();
+    println!("Hello, world!");
+    // shutdown();
+    println!("Hello, world!~~~~~~~~~~~~");
+
+    sys_exit(0);
 }
